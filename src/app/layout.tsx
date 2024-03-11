1d3b9f1fe13d9e1782470906bcb7bd239edf962c
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-03-11 20:50:53 +0700

    edit top nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="text-sans sm:max-w-lg mx-auto relative">
         <TopNavigation />
-        <main className="pt-14 px-4 pb-24">{children}</main>
+        <main className="pt-4 px-4 pb-24">{children}</main>
         <BottomNavigation />
       </body>
     </html>
--- src/components/pages/home/expense-activity/index.tsx
+++ src/components/pages/home/expense-activity/index.tsx
@@ -2,10 +2,10 @@ import ExpenseItem from "./expense-item";
 
 const ExpenseActivity = () => {
   return (
-    <>
-      <p className="mt-8 mb-4 text-sm sm:text-base text-slate-500">
-        Activity Today
-      </p>
+    <div className="relative">
+      <div className="sticky z-10 top-0 py-4 left-0 right-0 mt-4 text-sm sm:text-base text-slate-500 bg-white dark:bg-black">
+        <p className="">Activity Today</p>
+      </div>
       <div className="flex flex-col gap-3">
         <ExpenseItem />
         <ExpenseItem />
@@ -19,7 +19,7 @@ const ExpenseActivity = () => {
         <ExpenseItem />
         <ExpenseItem />
       </div>
-    </>
+    </div>
   );
 };
 
