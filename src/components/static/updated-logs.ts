58f3a8aec5a77a7ae99f17b7118c5a94e5d6ab62
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-03-14 12:10:31 +0700

    edit logs

--- src/components/static/updated-logs.ts
+++ src/components/static/updated-logs.ts
@@ -1,10 +1,8 @@
 import { TLogs } from "@/lib/types/logs-types";
 
 const logsData: TLogs = {
-  "Mon, March 11 2024": [
-    "Added add new activity today",
-    // "Added summary page screen",
-  ],
+  "Mon, March 14 2024": ["Added add new category modal today"],
+  "Mon, March 11 2024": ["Added add new activity modal today"],
   "Sat, March 09 2024": [
     "Initial Project Setup",
     "Added home page screen",
