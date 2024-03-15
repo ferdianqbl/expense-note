424c19bfe29f345593b5b2258a178080838a44c1
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-03-16 05:27:06 +0700

    edit page home

--- src/components/pages/home/expense-activity/expense-item.tsx
+++ src/components/pages/home/expense-activity/expense-item.tsx
@@ -15,10 +15,12 @@ const ExpenseItem = () => {
           <div className="flex gap-1 items-center w-3/4 overflow-hidden whitespace-nowrap">
             <p className="text-3xl">🎉</p>
             <div className="flex flex-col gap-1">
-              <p className="text-sm sm:text-base ">
+              <p className="text-sm sm:text-base">
                 Bought a new pair of shoes from Nikesssssssssss
               </p>
-              <p className="text-sm sm:text-base text-slate-400">Grocery</p>
+              <p className="text-sm sm:text-base text-slate-400 text-left">
+                Grocery
+              </p>
             </div>
           </div>
           <p className="text-sm sm:text-base font-bold text-red-500">
@@ -29,10 +31,20 @@ const ExpenseItem = () => {
       <DrawerContent>
         <DrawerHeader>
           <DrawerTitle>
-            <h3 className="text-xl font-medium">Add New Category</h3>
+            <h3 className="font-medium">Detail Activity</h3>
           </DrawerTitle>
         </DrawerHeader>
-        <p>test</p>
+        <div className="p-4 flex flex-col gap-2">
+          <p className="text-xl text-center sm:text-base font-bold text-red-500">
+            -100,000
+          </p>
+          <p className="text-sm text-center sm:text-base text-slate-400">
+            🎉 Grocery
+          </p>
+          <p className="text-sm sm:text-base">
+            Bought a new pair of shoes from Nikesssssssssss
+          </p>
+        </div>
       </DrawerContent>
     </Drawer>
   );
